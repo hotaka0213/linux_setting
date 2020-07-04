98730da9cceba267083ea6dcd5a3ae0968072b83
hotaka0213 <hotaka0213@yahoo.co.jp>
2020-07-04 17:57:53 +0900

    modified:   recovery.sh 	xprofile

--- recovery.sh
+++ recovery.sh
@@ -1,9 +1,12 @@
 #Neovim
-ln init.vim ~/.config/nvim/ 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln init.vim ../.config/nvim/init.vim 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
 #bash
 ln bashrc ../.bashrc
 ln colorrc ../.colorrc
+ln xprofile ../.xprofile
 #git
 ln gitconfig ../.gitconfig
